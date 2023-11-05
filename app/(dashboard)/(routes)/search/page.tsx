d0c9ef56d87fa24aa758b98e5a16fb4d4946c9d1
Gonçalo Pinto <goncalopintoneto@gmail.com>
2023-11-05 16:01:29 +0000

    Course Grid & Card UI

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -2,8 +2,11 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
-import { Categories } from "./_components/categories";
 import { SearchInput } from "@/components/search-input";
+import { getCourses } from "@/actions/get-courses";
+import { CoursesList } from "@/components/courses-list";
+
+import { Categories } from "./_components/categories";
 
 interface SearchPageProps {
   searchParams: {
@@ -13,10 +16,21 @@ interface SearchPageProps {
 }
 
 const SearchPage = async ({ searchParams }: SearchPageProps) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
   const categories = await db.category.findMany({
     orderBy: {
       name: "asc",
     },
+  });
+
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
   });
 
   return (
@@ -25,9 +39,8 @@ const SearchPage = async ({ searchParams }: SearchPageProps) => {
         <SearchInput />
       </div>
       <div className="p-6 space-y-4">
-        <Categories
-          items={categories}
-        />
+        <Categories items={categories} />
+        <CoursesList items={courses} />
       </div>
     </>
   );
