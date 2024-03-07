be149ee93c42576d8b47ed6914459b229a0eef53
devgoncalo <goncalopintoneto@gmail.com>
2024-03-07 16:19:28 +0000

    feat: Two Factor Authentication

--- auth.ts
+++ auth.ts
@@ -1,10 +1,11 @@
 import NextAuth from "next-auth";
 import { UserRole } from "@prisma/client";
-import authConfig from "./auth.config";
-
-import { getUserById } from "./data/user";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { db } from "./lib/db";
+
+import { db } from "@/lib/db";
+import authConfig from "@/auth.config";
+import { getUserById } from "@/data/user";
+import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
 
 export const {
   handlers: { GET, POST },
@@ -28,17 +29,30 @@ export const {
     async signIn({ user, account }) {
       // Allow OAuth without email verification
       if (account?.provider !== "credentials") {
-        return true;
-      }
+        return true
+      };
 
       const existingUser = await getUserById(user.id);
 
       // Prevent sign in without email verification
       if (!existingUser?.emailVerified) {
-        return false;
-      }
+        return false
+      };
 
-      // TODO: Add 2FA Check
+      if (existingUser.isTwoFactorEnabled) {
+        const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
+          existingUser.id
+        );
+
+        if (!twoFactorConfirmation) {
+          return false
+        };
+
+        // Delete two factor confirmation for next sign in
+        await db.twoFactorConfirmation.delete({
+          where: { id: twoFactorConfirmation.id },
+        });
+      }
 
       return true;
     },
